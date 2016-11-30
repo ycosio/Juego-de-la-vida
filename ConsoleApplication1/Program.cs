5b8ad11c1dae41ec58f40985a9efe6a73856525b
Yair <ycosio@ucol.mx>
2016-11-29 21:01:21 -0600

    Juego terminado

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
@@ -11,8 +12,9 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             bool[,] matriz = new bool[50,50];
+            bool[,] matriz2 = new bool[50, 50];
             //Llenar la matriz de muertos
-            for(int f=0;f<50;f++)
+            for (int f=0;f<50;f++)
             {
                 for (int c=0;c<50;c++) {
                     matriz[f, c] = false;
@@ -20,44 +22,77 @@ namespace ConsoleApplication1
             }
 
             //Asignar vivos inicales
-            matriz[40, 40]=true;
+            matriz[40, 40] = true;
             matriz[40, 41] = true;
             matriz[41, 40] = true;
             matriz[41, 41] = true;
+            matriz[41, 42] = true;
+            matriz[40, 39] = true;
 
-            //Hacer While infinito {
+            while (true) {
                 //Primer chequeo
                 for (int f = 0; f < 50; f++)
                 {
                     for (int c = 0; c < 50; c++)
                     {
+                        bool x;
+                        if (f == 40 && c==39)
+                            x = matriz[40, 39];
+
                         //Verificar que los valores no sobrepasen la matriz
-                        bool[] vector = new bool[8];
-                        if (f==0 && c==0)
-                            vector = { matriz[f, c - 1], matriz[f, c + 1], matriz[f + 1, c - 1], matriz[f + 1, c + 1], matriz[f + 1, c], matriz[f - 1, c - 1], matriz[f - 1, c + 1], matriz[f - 1, c] };
-                        else if(c==0)
-                            vector = { matriz[f, c - 1], matriz[f, c + 1], matriz[f + 1, c - 1], matriz[f + 1, c + 1], matriz[f + 1, c], matriz[f - 1, c - 1], matriz[f - 1, c + 1], matriz[f - 1, c] };
-                        else if (f == 0)
-                            vector = { matriz[f, c - 1], matriz[f, c + 1], matriz[f + 1, c - 1], matriz[f + 1, c + 1], matriz[f + 1, c], matriz[f - 1, c - 1], matriz[f - 1, c + 1], matriz[f - 1, c] };
+                        bool[] vector;
+                        if (f == 0 && c == 0)
+                            vector = new bool[3] { matriz[f, c + 1], matriz[f + 1, c + 1], matriz[f + 1, c] };
+                        else if (c == 0 && f!=49)
+                            vector = new bool[5] { matriz[f, c + 1], matriz[f + 1, c + 1], matriz[f + 1, c], matriz[f - 1, c + 1], matriz[f - 1, c] };
+                        else if (c == 0 && f == 49)
+                            vector = new bool[3] { matriz[f, c + 1], matriz[f - 1, c + 1], matriz[f - 1, c] };
+                        else if (f == 0 && c!=49)
+                            vector = new bool[5] { matriz[f, c - 1], matriz[f, c + 1], matriz[f + 1, c - 1], matriz[f + 1, c + 1], matriz[f + 1, c] };
+                        else if (f == 0 && c == 49)
+                            vector = new bool[3] { matriz[f, c - 1], matriz[f + 1, c - 1], matriz[f + 1, c] };
+                        else if (f == 49 && c == 49)
+                            vector = new bool[3] { matriz[f, c - 1], matriz[f - 1, c - 1], matriz[f - 1, c] };
+                        else if (c == 49)
+                            vector = new bool[5] { matriz[f, c - 1], matriz[f + 1, c - 1], matriz[f + 1, c], matriz[f - 1, c - 1], matriz[f - 1, c] };
+                        else if (f == 49)
+                            vector = new bool[5] { matriz[f, c - 1], matriz[f, c + 1], matriz[f - 1, c - 1], matriz[f - 1, c + 1], matriz[f - 1, c] };
                         else
-                            vector = { matriz[f, c - 1], matriz[f, c + 1], matriz[f + 1, c - 1], matriz[f + 1, c + 1], matriz[f + 1, c], matriz[f - 1, c - 1], matriz[f - 1, c + 1], matriz[f - 1, c] };
-                    
-                        //Validar las celular vecinas
-                        if (vida(matriz[f,c],vector))
-                            Console.Write("*");
+                            vector = new bool[8] { matriz[f, c - 1], matriz[f, c + 1], matriz[f + 1, c - 1], matriz[f + 1, c + 1], matriz[f + 1, c], matriz[f - 1, c - 1], matriz[f - 1, c + 1], matriz[f - 1, c] };
+                          //para cuando llega al 39,40 ya fue alterado anteriormente... se debe checar el reemplazo ya que matriz y matriz2 apuntan al mismo sitio de variable
+                        //Validar las celulas vecinas
+                        if (vida(matriz[f, c], vector))
+                        {
+                            matriz2[f, c] = true;
+                            Console.Write("■");
+                        }
                         else
+                        {
+                            matriz2[f , c] = false;
                             Console.Write(".");
+                        }
                     }
                     Console.WriteLine();
-
-                    //Modificar la matriz con los nuevos valores
                 }
-            //}
-            Console.ReadKey();
+
+                //Modificar la matriz con los nuevos valores
+                for (int i = 0; i < 50; i++)
+                {
+                    for (int j = 0; j < 50; j++)
+                    {
+                        matriz[i,j] = matriz2[i,j];
+                    }
+                }
+                //Console.ReadKey();
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
         }
 
         static bool vida(bool stateCell, bool[] cells)
         {
+
             int vivos = 0;
 
             //Contar muertos y vivos
@@ -68,14 +103,16 @@ namespace ConsoleApplication1
             }
 
             //Reglas
-            if (vivos < 2)
+            if (vivos < 2 && stateCell)
                 return false;
-            else if ((vivos == 2) || (vivos == 3))
+            else if (((vivos == 2) || (vivos == 3)) && stateCell==true)
                 return true;
             else if (vivos > 3 && stateCell)
                 return false;
-            else
+            else if (vivos >= 3 && !stateCell)
                 return true;
+            else
+                return false;
         }
     }
 }
